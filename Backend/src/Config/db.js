235ab210064c497c8467ec380fc2a0b5dea911c1
Moon Awan <malikmoon.developer061@gmail.com>
2025-08-20 15:22:07 +0500

    Update db.js

--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -4,9 +4,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log("DB error:", err));
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("DB error:", err.message);
+    process.exit(1);
+  }
 };
